34a5016b344c1ab9d5226c0bfda628bf6f83cd76
mvhf2001 <mvhf2001@hotmail.com>
2023-06-18 02:33:39 -0300

    gerger

---
+++ crud-devops-nw-main/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+        apiKey: "AIzaSyBwsajE6nklmcY5DCKnT2HNW_K3tV006Zo",
+        authDomain: "crud-fnr.firebaseapp.com",
+        databaseURL: "https://crud-fnr-default-rtdb.firebaseio.com",
+        projectId: "crud-fnr",
+        storageBucket: "crud-fnr.appspot.com",
+        messagingSenderId: "329335511641",
+        appId: "1:329335511641:web:ac8fd0cfd9a04a0b512f48",
+  }
+};
